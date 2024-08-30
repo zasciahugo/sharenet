9e9e99a1c3639449fcb3867bef1154b395a385ed
zasciah <zascia.hugo@nikham.com>
2024-08-30 12:41:08 +0200

    Update SpotPrices.vue

--- src/components/SpotPrices.vue
+++ src/components/SpotPrices.vue
@@ -4,43 +4,112 @@
     <table class="min-w-full bg-white border border-gray-300">
       <thead>
         <tr class="border-b border-gray-300">
-          <th class="w-1/5 px-4 py-2 text-left text-sm font-semibold border-r border-gray-300"
-            @click="sortTable('fullName')">Full Name</th>
-          <th class="w-1/5 px-4 py-2 text-left text-sm font-semibold border-r border-gray-300"
-            @click="sortTable('price')">Price</th>
-          <th class="w-1/5 px-4 py-2 text-left text-sm font-semibold border-r border-gray-300"
-            @click="sortTable('move')">Move</th>
-          <th class="w-1/5 px-4 py-2 text-left text-sm font-semibold border-r border-gray-300"
-            @click="sortTable('pmove')">Percentage Move</th>
-          <th class="w-1/5 px-4 py-2 text-left text-sm font-semibold" @click="sortTable('datetime')">Time</th>
+          <th
+            class="w-1/5 px-4 py-2 text-left text-sm font-semibold border-r border-gray-300 cursor-pointer"
+            @click="sortTable('fullName')"
+          >
+            Full Name
+            <span v-if="sortKey === 'fullName'">{{ sortOrder === 1 ? '▲' : '▼' }}</span>
+          </th>
+          <th
+            class="w-1/5 px-4 py-2 text-left text-sm font-semibold border-r border-gray-300 cursor-pointer"
+            @click="sortTable('price')"
+          >
+            Price
+            <span v-if="sortKey === 'price'">{{ sortOrder === 1 ? '▲' : '▼' }}</span>
+          </th>
+          <th
+            class="w-1/5 px-4 py-2 text-left text-sm font-semibold border-r border-gray-300 cursor-pointer"
+            @click="sortTable('move')"
+          >
+            Move
+            <span v-if="sortKey === 'move'">{{ sortOrder === 1 ? '▲' : '▼' }}</span>
+          </th>
+          <th
+            class="w-1/5 px-4 py-2 text-left text-sm font-semibold border-r border-gray-300 cursor-pointer"
+            @click="sortTable('pmove')"
+          >
+            Percentage Move
+            <span v-if="sortKey === 'pmove'">{{ sortOrder === 1 ? '▲' : '▼' }}</span>
+          </th>
+          <th
+            class="w-1/5 px-4 py-2 text-left text-sm font-semibold cursor-pointer"
+            @click="sortTable('datetime')"
+          >
+            Time
+            <span v-if="sortKey === 'datetime'">{{ sortOrder === 1 ? '▲' : '▼' }}</span>
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="spot in paginatedSpots" :key="spot.tickerId" class="border-b border-gray-200">
-          <td class="w-1/5 px-4 py-2 text-xs border-r border-gray-300">{{ spot.fullName }}</td>
-          <td class="w-1/5 px-4 py-2 text-xs border-r border-gray-300">{{ spot.price }}</td>
-          <td class="w-1/5 px-4 py-2 text-xs border-r border-gray-300">{{ spot.move }}</td>
-          <td class="w-1/5 px-4 py-2 text-xs border-r border-gray-300">{{ spot.pmove.toFixed(2) }}%</td>
-          <td class="w-1/5 px-4 py-2 text-xs">{{ new Date(spot.datetime).toLocaleString() }}</td>
+        <tr
+          v-for="spot in paginatedSpots"
+          :key="spot.tickerId"
+          class="border-b border-gray-200"
+        >
+          <td class="w-1/5 px-4 py-2 text-xs border-r border-gray-300">
+            {{ spot.fullName }}
+          </td>
+          <td class="w-1/5 px-4 py-2 text-xs border-r border-gray-300">
+            {{ spot.price }}
+          </td>
+          <td class="w-1/5 px-4 py-2 text-xs border-r border-gray-300">
+            {{ spot.move }}
+          </td>
+          <td class="w-1/5 px-4 py-2 text-xs border-r border-gray-300">
+            {{ spot.pmove.toFixed(2) }}%
+          </td>
+          <td class="w-1/5 px-4 py-2 text-xs">
+            {{ new Date(spot.datetime).toLocaleString() }}
+          </td>
         </tr>
       </tbody>
     </table>
 
     <!-- Pagination Controls -->
     <div class="mt-10 flex items-center justify-center">
-      <button @click="prevPage" :disabled="currentPage === 1" class="bg-blue-600 text-sm text-white p-2 rounded-full">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor"
-          class="size-5">
-          <path stroke-linecap="square" stroke-linejoin="square" d="M15.75 19.5 8.25 12l7.5-7.5" />
+      <button
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        class="bg-blue-600 text-sm text-white p-2 rounded-full"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2.5"
+          stroke="currentColor"
+          class="size-5"
+        >
+          <path
+            stroke-linecap="square"
+            stroke-linejoin="square"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          />
         </svg>
       </button>
-      <span class="mx-6 text-sm">Page <span class="font-medium">{{ currentPage }}</span> of <span class="font-medium">{{
-        totalPages }}</span></span>
-      <button @click="nextPage" :disabled="currentPage === totalPages"
-        class="bg-blue-600 text-sm text-white p-2 rounded-full">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-          class="size-5">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+      <span class="mx-6 text-sm">
+        Page <span class="font-medium">{{ currentPage }}</span> of
+        <span class="font-medium">{{ totalPages }}</span>
+      </span>
+      <button
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        class="bg-blue-600 text-sm text-white p-2 rounded-full"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m8.25 4.5 7.5 7.5-7.5 7.5"
+          />
         </svg>
       </button>
     </div>
@@ -82,8 +151,22 @@ const sortTable = (key) => {
 
 const sortedSpots = computed(() => {
   return spots.value.slice().sort((a, b) => {
-    if (a[sortKey.value] > b[sortKey.value]) return sortOrder.value;
-    if (a[sortKey.value] < b[sortKey.value]) return -sortOrder.value;
+    let aValue = a[sortKey.value];
+    let bValue = b[sortKey.value];
+
+    // Handle sorting for datetime
+    if (sortKey.value === 'datetime') {
+      aValue = new Date(aValue);
+      bValue = new Date(bValue);
+    }
+
+    // Handle sorting for numbers
+    if (typeof aValue === 'number' && typeof bValue === 'number') {
+      return (aValue - bValue) * sortOrder.value;
+    }
+
+    if (aValue > bValue) return sortOrder.value;
+    if (aValue < bValue) return -sortOrder.value;
     return 0;
   });
 });
